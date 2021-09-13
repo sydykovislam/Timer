85fe2911baa0c4ef305a0d06733ecaa46269d2dd
sydykovislam <sydykovislam03@gmail.com>
2021-09-13 23:23:48 +0600

    Stat project

---
+++ Timer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Sydykov Islam on 9/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBOutlet weak var hours: UILabel! 
+    
+    @IBOutlet weak var minutes: UILabel!
+    
+    @IBOutlet weak var seconds: UILabel!
+}
+
